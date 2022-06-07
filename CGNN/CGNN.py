583395340be90cd8f87f16c11a429b615d9fabea
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-06-07 11:23:04 +0300

    Update CGNN.py

--- CGNN/CGNN.py
+++ CGNN/CGNN.py
@@ -4,34 +4,19 @@ from scapy.layers.inet import TCP, Ether, IP, UDP
 from scapy.layers.dns import DNS
 import tensorflow as tf
 import tensorflow.keras as tfk
-import pandas as pd
 from sklearn.model_selection import train_test_split
 import keras.backend as K
+import pandas as pd
+from tensorflow.keras import activations
 import glob
-
-
-class SgcLayer(tfk.layers.Layer):
-    def __init__(self, outputsNumber):
-        super(SgcLayer, self).__init__()
-        self.outputs = outputsNumber
-        pass
-
-    def build(self, input_shape):
-        self.teta = self.add_weight("teta",
-                                    shape=[input_shape[-1], self.outputs],
-                                    trainable=True,
-                                    initializer="random_normal")
-
-    def call(self, inputs):
-        return tf.nn.relu(tf.matmul(inputs, self.teta))
-
-
-class cgnn_model(tf.keras.Model):
-    def __init__(self):
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+class cgnn_model(tfk.Model):
+    def __init__(self, n_classes=35):
         super(cgnn_model, self).__init__()
-        self.sgc1 = SgcLayer(516)
-        self.sgc2 = SgcLayer(256)
-        self.dense = tf.keras.layers.Dense(6)
+        self.sgc1 = SgcLayer(516, 1, 'relu')
+        self.sgc2 = SgcLayer(256, 1, 'relu')
+        self.dense = tf.keras.layers.Dense(n_classes)
 
     def call(self, inputs):
         x = self.sgc1(inputs)
@@ -42,49 +27,51 @@ class cgnn_model(tf.keras.Model):
         return x
 
 
-def get_diagonal_degree(A):
-    D = np.zeros((len(A), len(A)))
-    for i in range(len(A)):
-        D[i][i] = np.sum(A[i])
-    return D
+class SgcLayer(tfk.layers.Layer):
+    def __init__(self, outputsNumber, neighbourhood_distance=1, activation=None):
+        super(SgcLayer, self).__init__()
+        self.outputs = outputsNumber
+        self.S = None # local normalized adj matrix
+        self.neighbourhood_distance = neighbourhood_distance
+        self.activation = activations.get(activation)
 
+    def build(self, input_shape):
+        self.S = tf.Variable(
+            initial_value=np.linalg.matrix_power(
+                getS(input_shape[1]), 
+                self.neighbourhood_distance
+            ), 
+            trainable=False,
+            name='Normalized Adj. Matrix'
+        )
+        self.teta = self.add_weight(
+            "teta",
+            shape=[input_shape[2], self.outputs],
+            trainable=True,
+            initializer="random_normal"
+        )
 
-def sum_matrices(A, B):
-    result = np.array([[0 for x in range(len(A))] for y in range(len(A))])
-    for i in range(len(A)):
-        # iterate through columns
-        for j in range(len(A[0])):
-            result[i][j] = A[i][j] + B[i][j]
-    return np.array(result)
-
-
-def normalize_D_t(D_t):
-    for i in range(len(D_t)):
-        D_t[i][i] = 1 / np.sqrt(D_t[i][i])
-    return D_t
-
+    def call(self, inputs):
+        outputs = tf.matmul(self.S, float(inputs))
+        outputs = tf.matmul(outputs, self.teta)
+        if self.activation is not None:
+            outputs = self.activation(outputs)
+            
+        return outputs
 
 def getS(n):
     if n == 1:
         return np.array(0)
     if n == 2:
         return np.array([(0, 1),(1, 0)])
-    S = [[0 for x in range(n)] for y in range(n)]
-    S[0][0], S[n - 1][n - 1] = 0.50000, 0.50000
-    S[0][1], S[1][0], S[n - 2][n - 1], S[n - 1][n - 2] = 0.40825, 0.40825, 0.40825, 0.40825
-    S[1][1], S[1][2], S[n - 2][n - 2], S[n - 2][n - 3] = 0.33333, 0.33333, 0.33333, 0.33333
-    for i in range(2, len(S) - 2):
+    S = np.zeros((n,n), dtype='float32')
+    S[0,0], S[n - 1,n - 1] = 0.50000, 0.50000 # first and last vertices with self loop
+    S[0,1], S[1,0], S[n - 2,n - 1], S[n - 1,n - 2] = 0.40825, 0.40825, 0.40825, 0.40825 # first and last vertices with other edges
+    S[1,1], S[1,2], S[n - 2,n - 2], S[n - 2,n - 3] = 0.33333, 0.33333, 0.33333, 0.33333 # rest connected to first and last
+    for i in range(2, len(S) - 2): # rest of vertices
         for j in range(i - 1, i + 2):
-            S[i][j] = 0.33333
-    return np.asarray(S)
-
-
-def getSX(session):
-    X = np.array(session)
-    S = getS(len(session))
-    SX = np.dot(S, X)
-    del S, X
-    return np.asarray(SX)
+            S[i,j] = 0.33333
+    return S
 
 
 def complete(s, l=1500):
@@ -92,41 +79,23 @@ def complete(s, l=1500):
     return p[:1500]
 
 
-def calculate_A(session):
-    X = session
-    A = []
-    for i in range(len(X)):
-        temp = []
-        for j in range(len(X)):
-            if i == j + 1 or i == j - 1:
-                temp.append(1)
-            else:
-                temp.append(0)
-        A.append(temp)
-    return np.array(A)
-
-
-def createGraphFromSession(pcapName):
+def createGraphFromSession(pcapName): # return X features matrix
     try:
-        file = rdpcap(pcapName)
+        file = rdpcap(pcapName, count=300)
     except:
         return None
     l = []
-    counter = 0
     for p in file:
-        if counter == 100:  # here change
+        if len(l) > 99:
             break
-        counter += 1
         packet_proc = preprocessing(p)
         if packet_proc:
             if complete(packet_proc):
                 l.append(complete(packet_proc))
-        # if len(l) < 4:
-        #   return None
     del file, packet_proc
     if len(l) == 0:
         return None
-    return getSX(l)
+    return l
 
 
 def preprocessing(packet):
@@ -138,49 +107,20 @@ def preprocessing(packet):
             SYN = 0x02
             ACK = 0x10
             F = packet['TCP'].flags  # this should give you an integer
-            if F & FIN or F & SYN or F & ACK or packet.haslayer(DNS):
-                if len(packet) <= 66:
+            if F & FIN or F & SYN or F & ACK:
+                if len(packet[TCP].payload) == 0:
                     return None
 
             w_eth_header = bytes(packet)[14:]
             return w_eth_header
         elif packet.haslayer(UDP):
+            if packet.haslayer(DNS):
+                if len(packet[DNS].payload) == 0:
+                    return None
             w_eth_header = bytes(packet)[14:]
             zero_bytes = bytearray(12)
             new_packet = bytes(w_eth_header[:8]) + zero_bytes + bytes(w_eth_header[8:])
             return new_packet
-
-
-def getTrainTestGraphs():
-    df = pd.read_csv("data/FS zipped//id.csv")
-    train_name, test_name, train_label, test_label = train_test_split(df["fname"],
-                                                                      df["label"],
-                                                                      test_size=0.20,
-                                                                      random_state=42)
-    GraphsForTrain = []
-    GraphsForTest = []
-    counter = 0
-    for i in train_name:
-        print(counter)
-        g = createGraphFromSession(i)
-        counter += 1
-        # if g:
-        GraphsForTrain.append(np.ndarray.tolist(g))
-    LableForTrain = []
-    for i in train_label:
-        LableForTrain.append(int(i))
-
-    listOfLabelsForTest = []
-    for name in test_name:
-        g = createGraphFromSession(name)
-        print(name)
-
-        # if g:
-        GraphsForTest.append(np.ndarray.tolist(g))
-    for i in test_label:
-        listOfLabelsForTest.append(int(i))
-
-    return GraphsForTrain, list(LableForTrain), GraphsForTest, list(listOfLabelsForTest)  # the graphs is SX
 
 
 def f1(y_true, y_pred):
@@ -261,63 +201,63 @@ def main():
                 np.save(file + "_" + str(label), m)
             del m
 
-    # GraphsForTrain, LabelsForTrain, GraphsForTest, LabelsForTest = getTrainTestGraphs()
-    # m = cgnn_model()
-    # m.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-    #           loss='categorical_crossentropy', metrics=['acc', f1, precision, recall, TP, TN, FP, FN])
-    #
-    # max_n = max([len(i) for i in GraphsForTrain])
-    # for g in GraphsForTrain:
-    #     zeros = [0] * 1500
-    #     for i in range(max_n - len(g)):
-    #         g.append(zeros)
-    #
-    # graph_to_train = tf.convert_to_tensor(GraphsForTrain, tf.float32)
-    # graph_to_train = tf.cast(graph_to_train, tf.float32)
-    #
-    # dict_label = {}
-    # counter = 0
-    # for item in set(LabelsForTrain):
-    #     dict_label[item] = counter
-    #     counter += 1
-    #
-    # list_of_lables = []
-    # for item in LabelsForTrain:
-    #     list_temp = [0] * 6
-    #     list_temp[dict_label[item]] = 1
-    #     list_of_lables.append(list_temp)
-    #
-    # label_graph_to_train = tf.convert_to_tensor(list_of_lables)
-    # callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=20)
-    # m.fit(graph_to_train, np.reshape(label_graph_to_train, (1557, 1, 6)), epochs=10, callbacks=[callback],
-    #       batch_size=32)
-    # list_of_lables_test = []
-    # for item in LabelsForTest:
-    #     list_temp = [0] * 6
-    #     list_temp[dict_label[item]] = 1
-    #     list_of_lables_test.append(list_temp)
-    # max_n_Test = max([len(i) for i in GraphsForTest])
-    # for g in GraphsForTest:
-    #     zeros = [0] * 1500
-    #     for i in range(max_n_Test - len(g)):
-    #         g.append(zeros)
-    # graph_to_test = tf.convert_to_tensor(GraphsForTest, tf.float32)
-    # graph_to_test = tf.cast(graph_to_test, tf.float32)
-    #
-    # m.evaluate(graph_to_test, np.reshape(list_of_lables_test, (390, 1, 6)))
-    #
-    # a = np.array(m.predict(graph_to_test))
-    # idx = np.argmax(a, axis=-1)
-    # idx = idx.flatten()
-    # b = np.array(np.reshape(list_of_lables_test, (390, 1, 6)))
-    # _idx = np.argmax(b, axis=-1)
-    # _idx = _idx.flatten()
-    #
-    # result = tf.math.confusion_matrix(labels=_idx, predictions=idx,
-    #                                   num_classes=6)  # use one hot to convert to 1D to do confMatrix
-    #
-    # print(result)
+    files_names = glob.glob("./filtered_raw_dataset_temu2016_first_10_min/*/*.npy")
+    labels = [int(i.split("_")[-1][:5]) for i in files_names]
 
+    train_name, test_name, train_label, test_label = train_test_split(files_names, labels, test_size=0.2, random_state=42, stratify = labels)
+
+    classes = list(set(labels))
+    classes.sort()
+    train_l = []
+    for i in train_label:
+        v = [0] * len(classes)
+        v[classes.index(i)] = 1
+        train_l.append(v)
+
+    test_l = []
+    for i in test_label:
+        v = [0] * len(classes)
+        v[classes.index(i)] = 1
+        v[classes.index(i)] = 1
+        test_l.append(v)
+    train_l = tf.expand_dims(tf.constant(train_l), axis=1)
+    test_l = tf.expand_dims(tf.constant(test_l), axis=1)
+    print("before load")
+    # load the files
+    train = []
+    test = []
+    for i in train_name:
+        print("loading file: ", i)
+        train.append(np.load(i))
+    for i in test_name:
+        print("loading label: ", i)
+        test.append(np.load(i))
+    print("finished loading")
+    # convert to ragged tensor
+    train = tf.ragged.constant(train).to_tensor()
+    print("finished train ragged")
+    test = tf.ragged.constant(test).to_tensor()
+    print("finished test ragged")
+    # finished ragged
+    callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
+    m = cgnn_model()
+    m.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+              loss='categorical_crossentropy', metrics=['acc', f1, precision, recall, TP, TN, FP, FN])
+    m.fit(x=train, y=train_l, batch_size=32, epochs=400, shuffle = True, callbacks=[callback])
+    # test:
+    m.evaluate(test, test_l)
+
+    a = np.array(m.predict(test))
+    idx = np.argmax(a, axis=-1)
+    idx = idx.flatten()
+    b = np.array(test_l)
+    _idx = np.argmax(b, axis=-1)
+    _idx = _idx.flatten()
+
+    result = tf.math.confusion_matrix(labels=_idx, predictions=idx,
+                                      num_classes=35)  # use one hot to convert to 1D to do confMatrix
+    print(result)
+    # =========================================================================================
 
 if __name__ == "__main__":
     main()
